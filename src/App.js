7349b8d6004dbd55538d3664e5f91c465aa5b9b6
Cristi <cristianbazatin7@gmail.com>
2022-07-28 19:44:22 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+// function BackgroundImage() {
+//   return (
+//       <div id='image-background'> </div> 
+//   );
+// }
+
+function HeroComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='hero-component' id='image-background'>
+      <NavBar />
+      <div className="hero-content">
+        <HeroTitle />
+        <SearchBar />
+      </div>
     </div>
   );
 }
+
+function NavBar() {
+
+  return (
+
+    <div>
+      <img id='logo' src="https://www.drupal.org/files/project-images/pexels_logo_0.png" alt="logo" />
+      <h1 id="logo-name"><a id='txt-dec' href='https://www.pexels.com'>Pexels</a></h1>
+      <ul className='navbar'>
+        <li><a href="#explore">Explore</a></li>
+        <li><a href="#license">License</a></li>
+        <li><a href="#upload">Upload</a></li>
+        <li><a className='three-dots'></a></li>
+        <button className='button'>Join</button>
+      </ul>
+    </div>
+
+
+
+  );
+}
+
+
+function HeroTitle() {
+  return (
+    <div className='hero-title'>
+      <h1 className='home-text'>The best free stock photos, royalty free images &amp; videos shared by creators.</h1>
+    </div>
+  );
+}
+function SearchBar() {
+  return (
+    <div className='search-bar-container'>
+      <div className='search-bar'>
+        <input type='text' placeholder='Search for free photos' />
+        <button className='search-button'><i className="fa-solid fa-magnifying-glass" /></button>
+      </div>
+    </div>
+
+  );
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <HeroComponent />
+    </div>
+  );
+}
+
+
 
 export default App;
